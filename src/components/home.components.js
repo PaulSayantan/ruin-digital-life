bf11026026f3053f68e3c4fd4c3b1e663e995a6d
bconti123 <47548036+bconti123@users.noreply.github.com>
2020-09-18 23:36:40 -0700

    Added bryant-branch

---
+++ src/components/home.components.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <form>
+            <div class="home">
+                <h6>We build awesome CLI tools!!</h6>
+            </div>
+            </form>
+        );
+    }
+}
