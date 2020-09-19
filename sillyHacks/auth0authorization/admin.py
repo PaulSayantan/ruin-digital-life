206711e94cc5c19d8844ac04e60c35633614b72a
Pavit Kaur <pavitk1@gmail.com>
2020-09-20 03:21:48 +0530

    added django import export

--- sillyHacks/auth0authorization/admin.py
+++ sillyHacks/auth0authorization/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
 admin.site.register(CustomUser)
-admin.site.register(Image)
-admin.site.register(Thought)
+# admin.site.register(Image)
+# admin.site.register(Thought)
+
+class ThoughtResource(resources.ModelResource):
+    class Meta:
+        model=Thought
+
+class ImageResource(resources.ModelResource):
+    class Meta:
+        model=Image
+
+class ThoughtAdmin(ImportExportModelAdmin):
+    resource_class = ThoughtResource
+
+class ImageAdmin(ImportExportModelAdmin):
+    resource_class=ImageResource
+
+admin.site.register(Thought, ThoughtAdmin)
+admin.site.register(Image,ImageAdmin)
