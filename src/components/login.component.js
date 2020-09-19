86a61a7f9074c1abeec16454356e8de787cb93c6
vi_mi <fenil.jain2018@vitstudent.ac.in>
2020-09-20 00:36:09 +0530

    Completing integration and giving script for download

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+		constructor() {
+			super();
+			this.state = {
+				email: '',
+				pass: '',
+				twitterID: '',
+				show: false,
+			};
+		}
+
+	submitForm = () => {
+		var formdata = new FormData();
+				formdata.append("email", this.state.email);
+				formdata.append("password", this.state.pass);
+				formdata.append("twitterId", this.state.twitterID);
+
+		var requestOptions = {
+			method: 'POST',
+			body: formdata,
+			redirect: 'follow'
+		};
+
+		fetch("https://boiling-dawn-19818.herokuapp.com/api/register", requestOptions)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log('error', error));
+				console.log(this.state);
+		}
+
     render() {
         return (
             <form>
@@ -8,12 +37,29 @@ export default class Login extends Component {
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+									<input type="email" className="form-control" placeholder="Enter email" onChange={(e) => {
+										this.setState({
+											email: e.target.value,
+										});
+									}}/>
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+									<input type="password" className="form-control" placeholder="Enter password" onChange={(e) => {
+										this.setState({
+											pass: e.target.value,
+										});
+									}}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Twitter ID</label>
+									<input type="text" className="form-control" placeholder="Enter your twitter ID" onChange={(e) => {
+										this.setState({
+											twitterID: e.target.value,
+										});
+									}}/>
                 </div>
 
                 <div className="form-group">
@@ -23,10 +69,27 @@ export default class Login extends Component {
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+							<button type="button" className="btn btn-primary btn-block" onClick={() => {
+								//this.submitForm();
+			this.setState({
+				show: true
+				});
+							}}>Submit</button>
+							{
+								this.state.show
+									?
+										<a href = "../../script.sh" download><button type="button" style={{marginTop: "10px"}} className="btn btn-primary btn-block" onClick={() => {
+							}}>Download</button></a>
+									:
+									<div></div>
+							}
+							{
+								/*
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
+								*/
+							}
             </form>
         );
     }
